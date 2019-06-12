3629e548cdb6ba3e0fe0d463954a874cbdaaefd1
Aditya2 <aditya2@localhost.localdomain>
2019-06-12 20:50:46 +0530

    Initial Commit

---
+++ Project/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for Project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os, sys
+sys.path.append('/opt/bitnami/apps/django/django_projects/Project')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/Project/egg_cache")
+
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
+
+application = get_wsgi_application()
